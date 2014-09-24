b7c0fecc6757cf7f7fe1814bbfb2d6cbbf29da4b
Christian Dalsvaag <chridal@gmail.com>
2014-09-24 17:31:14 +0200

    Added more tests to users_spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,55 +4,74 @@ RSpec.describe "Users", :type => :request do
 
   let(:user1) { FactoryGirl.create(:user) }
   let(:user2) { FactoryGirl.create(:user) }
+  
+  let(:full_user_response) {
+    { 'user' =>
+      { 'id' => user1.id,
+        'username' => user1.username,
+        'display_name' => user1.display_name,
+        'availability' => user1.availability,
+        'phone_number' => user1.phone_number,
+        'private_profile' => user1.private_profile }
+    }.to_json
+  }
+  
+  let(:public_user_response){
+      { 'user' =>
+          { 'id' => user1.id,
+          'username' => user1.username,
+          'display_name' => user1.display_name,
+          'private_profile' => false }
+    }.to_json
+  }
+
+  let(:private_user_response){
+    { 'user' =>
+      { 'id' => user1.id,
+        'username' => user1.username,
+        'private_profile' => user1.private_profile }
+    }.to_json
+  }
 
   describe "GET /user/id" do 
 
-    context "user is private" do
-
-      context "requester is not a followee of user" do
-
-        it "should return limited amount of data" do
-          expected_response =
-            { 'user' =>
-              { 'id' => user1.id,
-                'username' => user1.username,
-                'private_profile' => user1.private_profile }
-            }.to_json
-
-            get "user/#{user1.id}", nil, { 'X-AUTH-TOKEN' => user1.session.auth_token}
-          expect(response.body).to be_json_eql(expected_response)
-        end 
-
-      end
-
-      context "requester is a followee of user" do
-
+    context "as a followee" do
+      context "user is private" do
         it "should return full json body" do
-          expected_response =
-            { 'user' =>
-              { 'id' => user1.id,
-                'username' => user1.username,
-                'display_name' => user1.display_name,
-                'availability' => user1.availability,
-                'phone_number' => user1.phone_number,
-                'private_profile' => user1.private_profile }
-            }.to_json
 
-          # TODO: This call should be stubbed out
-            post "user/#{user1.id}/follow/#{user2.id}", nil, { 'X-AUTH-TOKEN' => user1.session.auth_token}
-            get "user/#{user1.id}", nil, { 'X-AUTH-TOKEN' => user2.session.auth_token }
+          # TODO: post call should be stubbed out
+          post "user/#{user1.id}/follow/#{user2.id}", nil, { 'X-AUTH-TOKEN' => user1.session.auth_token}
+          get "user/#{user1.id}", nil, { 'X-AUTH-TOKEN' => user2.session.auth_token }
 
-          expect(response.body).to be_json_eql(expected_response)
+          expect(response.body).to be_json_eql(full_user_response)
         end
+      end
+    end
 
+    context "without relation" do
+      context "user is private" do
+        it "should return limited amount of data" do
+            get "user/#{user1.id}", nil, { 'X-AUTH-TOKEN' => user2.session.auth_token}
+            expect(response.body).to be_json_eql(private_user_response)
+        end 
       end
 
+      context "user is public" do
+        it "should return full json body, except phone number and availiability" do
+          user1.update_attributes(private_profile: false)
+
+          get "user/#{user1.id}", nil, { 'X-AUTH-TOKEN' => user2.session.auth_token }
+          
+          expect(response.body).to be_json_eql(public_user_response)
+        end
+      end
     end
 
-    describe "user is public" do
+  end
 
+  context "user is requesting his own profile" do
+    it "should return full json body" do
     end
-
   end
 
 end
