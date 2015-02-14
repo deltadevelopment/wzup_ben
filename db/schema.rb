2a370fa355e415b306b5e888f25c20afa84cbae1
Christian Dalsvaag <christian@ddev.no>
2015-02-14 13:55:23 +0100

    Added default value 0 to attending

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214122730) do
+ActiveRecord::Schema.define(version: 20150214125401) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 20150214122730) do
     t.integer  "user_id"
     t.integer  "event_id"
     t.integer  "invitee_id"
-    t.integer  "attending"
+    t.integer  "attending",  default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
   end
