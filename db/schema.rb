5dc66d53885e8302173e4ed6183362e478b6ce57
Christian Dalsvaag <christian@ddev.no>
2015-02-14 11:20:25 +0100

    Changed event.private to default to true

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214090132) do
+ActiveRecord::Schema.define(version: 20150214100346) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20150214090132) do
     t.string   "place"
     t.string   "location"
     t.datetime "time"
-    t.boolean  "private"
+    t.boolean  "private",     default: true
     t.integer  "degrees"
     t.integer  "user_id"
     t.datetime "created_at"
