abc25cebb76f7fd0c51d16c83e89eb55571c5eda
Christian Dalsvaag <christian@ddev.no>
2015-02-14 10:24:37 +0100

    Added migration, and migrated table for events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141001142028) do
+ActiveRecord::Schema.define(version: 20150214090132) do
+
+  create_table "events", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "place"
+    t.string   "location"
+    t.datetime "time"
+    t.boolean  "private"
+    t.integer  "degrees"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "following_requests", force: true do |t|
     t.integer  "user_id"
