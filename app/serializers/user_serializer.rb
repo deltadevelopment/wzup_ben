1483bd3062c74ee6e000e1328b52cc0e184bac82
Christian Dalsvaag <christian@ddev.no>
2015-03-19 16:43:26 +0100

    BEN-98 #comment Serializer had become too secretive after last fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,14 @@
 class UserSerializer < ActiveModel::Serializer 
- attributes :id, :username, :private_profile, :display_name, :availability, :is_followee
+ attributes :id, :username, :private_profile, :display_name, :availability, :is_followee, :email, :phone_number
   
   # TODO: Should the status be included with the JSON for the user?
   # has_one :status
 
   def filter(keys)
-    if owner_or_follower or !object.has_private_profile?
+    if owner
       keys
+    elsif scope.is_follower(object.id) or !object.has_private_profile?
+      keys - [:phone_number, :email]
     else
       keys - [:availability, :display_name]
     end
