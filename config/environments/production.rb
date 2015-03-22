b818ecbdaa248ebce7071306b8b638f60033b2f8
Christian Dalsvaag <christian@ddev.no>
2015-03-22 13:20:49 +0100

    More stupid typos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
-  Resque.config do |config|
+  Resque.configure do |config|
     config.redis = ENV['REDISCLOUD_URL']
   end
 
