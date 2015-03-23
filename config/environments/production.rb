e5c0d50a8c7e83638766dd094d59b41c3a90cacc
Christian Dalsvaag <christian@ddev.no>
2015-03-23 16:49:36 +0100

    Trying to fix deploy bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
-  Resque.configure do |config|
-    config.redis = ENV['REDISCLOUD_URL']
-  end
-
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
